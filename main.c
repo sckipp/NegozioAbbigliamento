a5006223a1047577f3355711fa56005273ac5b76
riioKen <domenicovitale69@gmail.com>
2022-04-19 18:26:37 +0200

    primo commit

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 /*
- * TO - DO C'E' UN COMMENTO AL LOGIN UTENTE DA CONTROLLARE.
- * IL FREELISTA CONTINUO MI ROMPE IL PROGRAMMA, CERCARE DI RISOLVERE ASAP
+ * TO - DO SE FACCIO LOGIN ED ESCO DALL'ACCOUNT, SE RIOLGGO NON MI FA SCEGLIERE L'ACCOUNT (NECESSARIA FREE DI UTENTELOGGATO)
  *
  */
 #include <stdio.h>
@@ -18,6 +17,7 @@ typedef struct accountUtente_Lista{
 }accountUtenteLista;
 
 typedef struct abbigliamentoLista{
+    int id;
     char *nomeAbbigliamento;
     int taglieDisp[TAGLIE];
     float prezzo;
@@ -25,6 +25,7 @@ typedef struct abbigliamentoLista{
 }abbigliamentoLista;
 
 typedef struct scarpelista{
+    int id;
     char *nomeScarpe;
     int numeroDisp[10];
     float prezzo;
@@ -39,15 +40,19 @@ void registrazioneUtente();
 accountUtenteLista *loginUtente (accountUtenteLista  *utenteLoggato, char *nomeUtente, char *password, accountUtenteLista *accountLista);
 accountUtenteLista *inserimentoAccountLista (accountUtenteLista *accountLista);
 accountUtenteLista *freeAccount (accountUtenteLista *testa);
+abbigliamentoLista *freeAbbigl (abbigliamentoLista *testa);
+scarpeLista *freeScarpe (scarpeLista *testa);
 accountUtenteLista *confrontoCredenzialiConDB (accountUtenteLista *accountLista, char *nomeUtente, char *password);
 void *operazioni (accountUtenteLista *utenteLoggato);
 void allocazioneAbbigliamentoLista (char *nome,  abbigliamentoLista *tmp);
 void allocazioneScarpeLista (char *nome, scarpeLista *tmp);
-
+void stampaScarpeLista (scarpeLista *scarpe);
+void stampaAbbigliamentoLista (abbigliamentoLista *abbigliamento);
 //FILE funzioni.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 //GESTIONE DELLE LISTE
 //lista degli account
@@ -88,83 +93,6 @@ accountUtenteLista *inserimentoAccountLista (accountUtenteLista *accountLista){
     fclose(fp);
     return accountLista;
 }
-//lista dell'abbigliamento
-abbigliamentoLista *creaNodoAbbigliamento (char *nomeAbbigliamento, float prezzo){
-    abbigliamentoLista *tmp = NULL;
-    tmp = malloc (sizeof (abbigliamentoLista));
-    if(tmp == NULL)
-        return NULL;
-    else{
-        tmp ->next = NULL;
-        allocazioneAbbigliamentoLista(nomeAbbigliamento, tmp);
-        strcpy(tmp->nomeAbbigliamento,nomeAbbigliamento);
-        tmp ->prezzo = prezzo;
-    }
-    return tmp;
-}
-abbigliamentoLista *inserimentoTestaAbbigliamento (abbigliamentoLista *abbigliamento, char *nomeAbbigliamento, float prezzo){
-    abbigliamentoLista *tmp = NULL;
-
-    if(abbigliamento == NULL)
-        return creaNodoAbbigliamento(nomeAbbigliamento, prezzo);
-    tmp = creaNodoAbbigliamento(nomeAbbigliamento, prezzo);
-    tmp ->next = abbigliamento;
-    return tmp;
-}
-abbigliamentoLista *popolamentoAbbigliamentoLista(abbigliamentoLista *abbigliamento){
-    FILE *fp = NULL;
-    char stringa[100];
-    char nome[100];
-    float prezzo;
-
-    fp = fopen("abiti.txt", "r");
-    while(fgets(stringa, 100, fp) != NULL){
-        sscanf(stringa, "%f %[^\n]s", &prezzo, nome);
-        abbigliamento = inserimentoTestaAbbigliamento(abbigliamento,nome, prezzo);
-    }
-    return abbigliamento;
-}
-
-//lista delle scarpe
-scarpeLista *creaNodoScarpe (char *nomeScarpe, float prezzo){
-    scarpeLista *tmp = NULL;
-    tmp = malloc (sizeof (scarpeLista));
-    if(tmp == NULL)
-        return NULL;
-    else{
-        tmp ->next = NULL;
-        allocazioneScarpeLista(nomeScarpe, tmp);
-        strcpy(tmp->nomeScarpe,nomeScarpe);
-        tmp ->prezzo = prezzo;
-        printf("\n\nTEST TMP PREZZO %.2f", tmp->prezzo);
-    }
-    return tmp;
-}
-scarpeLista *inserimentoTestaScarpe (scarpeLista *scarpe, char *nomeScarpe, float prezzo){
-    scarpeLista *tmp = NULL;
-
-    if(scarpe == NULL)
-        return creaNodoScarpe(nomeScarpe, prezzo);
-    tmp = creaNodoScarpe(nomeScarpe, prezzo);
-    tmp ->next = scarpe;
-    return tmp;
-}
-scarpeLista *popolamentoScarpeLista(scarpeLista *scarpe){
-    FILE *fp = NULL;
-    char stringa[100];
-    char nome[100];
-    float prezzo;
-
-    fp = fopen("scarpe.txt", "r");
-    while(fgets(stringa, 100, fp) != NULL){
-        sscanf(stringa, "%f %[^\n]s", &prezzo, nome);
-        printf("\n\nTEST 2 %.2f", prezzo);
-        scarpe = inserimentoTestaScarpe(scarpe,nome, prezzo);
-    }
-    return scarpe;
-}
-
-//Da aggiustare
 void allocazioneAccountUtenteLista (char *nome, char *password, accountUtenteLista *tmp){
     int dimN = 0, dimP = 0;
     dimN = strlen(nome);
@@ -173,59 +101,8 @@ void allocazioneAccountUtenteLista (char *nome, char *password, accountUtenteLis
     tmp ->password = malloc(dimP * sizeof (char));
 
 }
-void allocazioneAbbigliamentoLista (char *nome,  abbigliamentoLista *tmp){
-    int dimN = 0;
-    dimN = strlen(nome);
-    tmp ->nomeAbbigliamento = malloc(dimN * sizeof (char));
-
-}
-void allocazioneScarpeLista (char *nome, scarpeLista *tmp){
-    int dimN = 0;
-    dimN = strlen(nome);
-    tmp ->nomeScarpe = malloc(dimN * sizeof (char));
-
-}
-
-//Da aggiustare
-void stampaAccountLista (accountUtenteLista *accountLista){
-    printf("\n\n\nStampa della lista\n\n\n\n"); //Debugging purpose
-    while(accountLista != NULL){
-        printf("Nome Utente %s Password %s\n", accountLista ->nomeUtente, accountLista ->password);
-        accountLista = accountLista ->next;
-    }
-    printf("\n\n\n");
-} //Da cancellare
-
-void stampaScarpeLista (scarpeLista *scarpe){
-    printf("\nStampa della lista scarpe\n"); //Debugging purpose
-    while(scarpe != NULL){
-        printf("%s %.2f\n", scarpe ->nomeScarpe, scarpe ->prezzo);
-        scarpe = scarpe ->next;
-    }
-    printf("\n");
-}
-void stampAbbigliamentoLista (abbigliamentoLista *abbigliamento){
-    printf("\nStampa della lista abbigliamento\n"); //Debugging purpose
-    while(abbigliamento != NULL){
-        printf("%s %.2f\n", abbigliamento ->nomeAbbigliamento, abbigliamento ->prezzo);
-        abbigliamento = abbigliamento ->next;
-    }
-    printf("\n");
-}
-
-//free delle liste
 accountUtenteLista *freeAccount (accountUtenteLista *testa){
     accountUtenteLista *tmp = NULL;
-
-        while (testa != NULL) {
-            tmp = testa;
-            testa = testa->next;
-            free(tmp);
-        }
-        return NULL;
-}
-scarpeLista *freeScarpe (scarpeLista *testa){
-    scarpeLista *tmp = NULL;
 
     while (testa != NULL) {
         tmp = testa;
@@ -233,6 +110,70 @@ scarpeLista *freeScarpe (scarpeLista *testa){
         free(tmp);
     }
     return NULL;
+}
+
+//lista dell'abbigliamento
+abbigliamentoLista *creaNodoAbbigliamento (char *nomeAbbigliamento, float prezzo, int id){
+    /*srand(time(NULL));
+    rand();*/
+    abbigliamentoLista *tmp = NULL;
+    tmp = malloc (sizeof (abbigliamentoLista));
+    if(tmp == NULL)
+        return NULL;
+    else{
+        tmp ->next = NULL;
+        allocazioneAbbigliamentoLista(nomeAbbigliamento, tmp);
+        strcpy(tmp->nomeAbbigliamento,nomeAbbigliamento);
+        tmp ->id = id;
+        tmp ->prezzo = prezzo;
+        /*for(int i = 0; i < TAGLIE; i++)
+            tmp ->taglieDisp[i] = rand()%5;*/
+    }
+    return tmp;
+}
+abbigliamentoLista *inserimentoCodaAbbigliamento (abbigliamentoLista *abbigliamento, char *nomeAbbigliamento, float prezzo, int id){
+    abbigliamentoLista *tmp = abbigliamento;
+
+    if(tmp == NULL)
+        return creaNodoAbbigliamento(nomeAbbigliamento, prezzo, id);
+    while(tmp ->next != NULL)
+        tmp = tmp ->next;
+    tmp -> next = creaNodoAbbigliamento(nomeAbbigliamento, prezzo, id);
+    return abbigliamento;
+}
+void allocazioneAbbigliamentoLista (char *nome,  abbigliamentoLista *tmp){
+    int dimN = 0;
+    dimN = strlen(nome);
+    tmp ->nomeAbbigliamento = malloc(dimN * sizeof (char));
+
+}
+abbigliamentoLista *popolamentoAbbigliamentoLista(abbigliamentoLista *abbigliamento, int *contatore){
+    FILE *fp = NULL;
+    char stringa[100];
+    char nome[100];
+    float prezzo;
+    int id =0;
+    fp = fopen("abiti.txt", "r");
+    abbigliamento = freeAbbigl(abbigliamento);
+    while(fgets(stringa, 100, fp) != NULL){
+        sscanf(stringa, "%f %[^\n]s", &prezzo, nome);
+        id++;
+        abbigliamento = inserimentoCodaAbbigliamento(abbigliamento, nome, prezzo, id);
+    }
+    *contatore = id;
+    return abbigliamento;
+}
+void stampaAbbigliamentoLista (abbigliamentoLista *abbigliamento){
+    char tagl[]={'S', 'M', 'L', 'L'};
+    printf("\nStampa della lista abbigliamento\n"); //Debugging purpose
+    while(abbigliamento != NULL){
+        printf("%d - %s %.2f\n",abbigliamento ->id, abbigliamento ->nomeAbbigliamento, abbigliamento ->prezzo);
+        /*for(int i  = 0; i < TAGLIE; i++)
+            printf("%c: %d", tagl[i], abbigliamento ->taglieDisp[i]);
+        printf("\n");*/
+        abbigliamento = abbigliamento ->next;
+    }
+    printf("\n");
 }
 abbigliamentoLista *freeAbbigl (abbigliamentoLista *testa){
     abbigliamentoLista *tmp = NULL;
@@ -245,6 +186,73 @@ abbigliamentoLista *freeAbbigl (abbigliamentoLista *testa){
     return NULL;
 }
 
+//lista delle scarpe
+scarpeLista *creaNodoScarpe (char *nomeScarpe, float prezzo, int id){
+    scarpeLista *tmp = NULL;
+    tmp = malloc (sizeof (scarpeLista));
+    if(tmp == NULL)
+        return NULL;
+    else{
+        tmp ->next = NULL;
+        allocazioneScarpeLista(nomeScarpe, tmp);
+        strcpy(tmp->nomeScarpe,nomeScarpe);
+        tmp ->id = id;
+        tmp ->prezzo = prezzo;
+    }
+    return tmp;
+}
+scarpeLista *inserimentoCodaScarpe (scarpeLista *scarpe, char *nomeScarpe, float prezzo, int id){
+    scarpeLista *tmp = scarpe;
+
+    if(scarpe == NULL)
+        return creaNodoScarpe(nomeScarpe, prezzo, id);
+    while(tmp ->next != NULL)
+        tmp = tmp ->next;
+    tmp -> next = creaNodoScarpe(nomeScarpe, prezzo, id);
+    return scarpe;
+}
+scarpeLista *popolamentoScarpeLista(scarpeLista *scarpe, int contatore){
+    FILE *fp = NULL;
+    char stringa[100];
+    char nome[100];
+    float prezzo;
+    int id = contatore;
+    printf("stampa contatore %d", contatore);
+    fp = fopen("scarpe.txt", "r");
+    scarpe = freeScarpe(scarpe);
+    while(fgets(stringa, 100, fp) != NULL){
+        sscanf(stringa, "%f %[^\n]s", &prezzo, nome);
+        contatore++;
+        scarpe = inserimentoCodaScarpe(scarpe, nome, prezzo, contatore);
+    }
+    return scarpe;
+}
+void stampaScarpeLista (scarpeLista *scarpe){
+    printf("\nStampa della lista scarpe\n"); //Debugging purpose
+    while(scarpe != NULL){
+        printf("%d - %s %.2f\n",scarpe ->id, scarpe ->nomeScarpe, scarpe ->prezzo);
+        scarpe = scarpe ->next;
+    }
+    printf("\n");
+}
+void allocazioneScarpeLista (char *nome, scarpeLista *tmp){
+    int dimN = 0;
+    dimN = strlen(nome);
+    tmp ->nomeScarpe = malloc(dimN * sizeof (char));
+
+}
+scarpeLista *freeScarpe (scarpeLista *testa){
+    scarpeLista *tmp = NULL;
+
+    while (testa != NULL) {
+        tmp = testa;
+        testa = testa->next;
+        free(tmp);
+    }
+    return NULL;
+}
+
+//gestione account utente
 accountUtenteLista *confrontoCredenzialiConDB (accountUtenteLista *accountLista, char *nomeUtente, char *password){
     accountUtenteLista *tmp = accountLista;
 
@@ -265,7 +273,6 @@ void popolamentoFileAccount(char *nome, char *password){
     fprintf(fp, "%s %s\n", nome, password);
     fclose(fp);
 }
-
 void registrazioneUtente(){
 
     char nome[MAX], password[MAX];
@@ -321,7 +328,7 @@ void gestioneMagazzino(){
 
 }
 
-//Gestione interfaccia
+//GESTIONE INTERFACCIA
 accountUtenteLista *loginUtente (accountUtenteLista  *utenteLoggato, char *nomeUtente, char *password, accountUtenteLista *accountLista){
 
     printf("Inserisci lo username: ");
@@ -342,25 +349,48 @@ accountUtenteLista *loginUtente (accountUtenteLista  *utenteLoggato, char *nomeU
         return NULL;
     }
 }
+void acquistoProdotti(abbigliamentoLista *abbigliamento, scarpeLista *scarpe, int contatore){
+    int scelta;
+    abbigliamentoLista *tmpAbbigliamento = abbigliamento;
+    scarpeLista *tmpScarpe = scarpe;
+
+    printf("Scelta:");
+    scanf("%d", &scelta);
+
+    if(scelta <= contatore) {
+        while (tmpAbbigliamento != NULL) {
+            if (scelta == tmpAbbigliamento->id)
+                printf("Hai acquistato %s", tmpAbbigliamento->nomeAbbigliamento);
+            tmpAbbigliamento = tmpAbbigliamento->next;
+        }
+    }else{
+        while(tmpScarpe != NULL){
+            if(scelta == tmpScarpe ->id)
+                printf("Hai acquistato %s", tmpScarpe ->nomeScarpe);
+            tmpScarpe = tmpScarpe ->next;
+        }
+    }
+}
 void *operazioni (accountUtenteLista *utenteLoggato){
 
     scarpeLista *scarpe = NULL;
     abbigliamentoLista  *abbigliamento = NULL;
-
+    int contatore;
     int scelta;
     float importo;
     do {
         printf("1 - Mostra capi d'abbigliamento\n2 - Ricarica il tuo conto virtuale\n3 - Preleva soldi dal tuo conto virtuale\n"
-               "4 - Carrello\n0 - Per tornare alla home\nInserisci operazione : ");
+               "4 - Carrello\n0 - Logout\nInserisci operazione : ");
         scanf("%d", &scelta);
         fflush(stdin);
         switch (scelta) {
             case 1:
                 printf("DEBUGGING PURPOSE\n\n\n");
-                abbigliamento = popolamentoAbbigliamentoLista(abbigliamento);
-                scarpe = popolamentoScarpeLista(scarpe);
-                stampAbbigliamentoLista(abbigliamento);
+                abbigliamento = popolamentoAbbigliamentoLista(abbigliamento, &contatore);
+                scarpe = popolamentoScarpeLista(scarpe, contatore);
+                stampaAbbigliamentoLista(abbigliamento);
                 stampaScarpeLista(scarpe);
+                acquistoProdotti(abbigliamento, scarpe, contatore); //INSERIRE FUNZIONE DI ACQUISTO ARTICOLO
                 break;
             case 2:
                 printf("Inserisci l'importo");
@@ -376,17 +406,17 @@ void *operazioni (accountUtenteLista *utenteLoggato){
                     printf("Impossibile completare l'operazione...\nBilancio non sufficiente\n");
                 break;
             }
-            case -1:
-                printf("Annullamento dell'operazione\n");
+            case 0:
+                printf("Logout dall'account in corso...\n");
                 break;
             default:
                 printf("Hai inserito un operazione sconosciuta...\nReinserire l'operazione\n");
         }
         system("pause");
-    }while(scelta != -1);
+    }while(scelta != 0);
 
-    //scarpe = freeScarpe(scarpe);
-    //abbigliamento = freeAbbigl(abbigliamento);
+     //scarpe = freeScarpe(scarpe);
+    // abbigliamento = freeAbbigl(abbigliamento);
 }
 
 //FILE main.c
